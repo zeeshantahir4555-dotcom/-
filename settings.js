28e7330e7b796a40777e6a83e2a125d3e822630a
zeeshantahir4555-dotcom <zeeshantahir4555@gmail.com>
2026-01-20 18:31:29 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𓆩 乙ᗴᗴᔕᕼᗩᑎ 丅ᗩᕼᎥᖇ 𓆪',
+  author: '‎',
+  botName: "𓆩 乙ᗴᗴᔕᕼᗩᑎ 丅ᗩᕼᎥᖇ 𓆪",
+  botOwner: 'ZEESHAN', // Your name
+  ownerNumber: '923337739296', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/zeeshantahir4555-dotcom/-",
+};
+
+module.exports = settings;
